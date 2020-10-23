cc3fc7123f7f86d05609da75bc3075fe50f3241b
JVRossier <73343229+JVRossier@users.noreply.github.com>
2020-10-23 15:56:30 +0200

    Initialisation de ceasar
    Le fichier test m'a permi de faire des test sur mon code
    J'ai codé premièrement un tableau qui utilise la key
    En suite j'ai codé un deuxième tableau (j'aurai utiliser le meme tableau qu'avant) pour que ce soit plus clair
    Par contre dans cette deuxième partie du code je ne comprend pas pourquoi Helper.bytesToString ne fonctionne pas (ligne 41 dans la fichier test)
    J'ai mis quelques ligne de texte qui te permettent de comprendre mon code
    Si tu trouve certaine chose qui manque dans mon code dis moi :)

--- src/crypto/Encrypt.java
+++ src/crypto/Encrypt.java
@@ -45,9 +45,50 @@ public class Encrypt {
 	 */
 	public static byte[] caesar(byte[] plainText, byte key, boolean spaceEncoding) {
 		assert(plainText != null);
-		// TODO: COMPLETE THIS METHOD
 		
-		return null; // TODO: to be modified
+		
+		//Création tableau qui va changer les byte
+		 byte tab [] = new byte[plainText.length];
+		 	
+		 //Remplissage du tableau 
+		 for(int i=0; i<plainText.length; i++) {
+			 	
+		//si l'on code l'espace "  "
+			 	if(spaceEncoding){
+				 
+				 tab[i]= (byte) (plainText[i] + (byte)key); 	
+				 
+			 	} else {
+		//si l'on ne code pas les epaces	 
+				 	if(plainText[i] != 32) {
+			
+				 		tab[i]= (byte) (plainText[i] + (byte)key);
+				 	} else { 
+				 		
+				 		tab[i]=32;
+				 	}
+			}
+		 }
+		 
+		 //Création tableau qui retransforme les valeurs de tab en String
+		 byte [] tabReturn = new byte [plainText.length];
+		 
+		 //Remplissage du Tableau
+		 for(int j=0;j<plainText.length;j++) {
+			 		 
+		 tabReturn [j] = Helper.bytesToString(tab[j]);
+		 }		 			 		 
+		
+		 		 	
+		 //Valeur attendue pour le cipherText: {-101, 32, -87, -109, -96, -90}
+			
+		 assert(plainText != null);
+			// TODO: COMPLETE THIS METHOD
+			
+			return tab; // TODO: to be modified
+		
+		
+		
 	}
 	
 	/**
--- test.java
+++ test.java
@@ -1,5 +1,53 @@
-public class test {
-  salut
-  salut
+import crypto.Helper;
 
+public class test {
+	 public static void main(String[] args) {
+		 				 
+		 String plainText = "bonne journée";
+		 System.out.println(plainText);
+		  byte[] plainBytes = Helper.stringToBytes(plainText);
+		  
+		  
+		 byte Key = 4;
+		 byte[] cipherText = caesar(plainBytes , Key);
+		 
+		 for(int i=0; i<plainText.length(); i++) {
+			 		
+		 System.out.print("  "+cipherText[i]);
+		  }
+		 
+	 }
+	 public static byte[] caesar(byte [] plainText, byte key) {
+		 
+		 //Création tableau qui va changer les byte
+		 byte tab [] = new byte[plainText.length];
+		 	
+		 //Remplissage du tableau 
+		 for(int i=0; i<plainText.length; i++) {
+			 			 			 
+			 if(plainText[i] != 32) {
+				 tab[i]= (byte) (plainText[i] + (byte)key);
+			 } else { 
+				 tab[i]=32;
+			 }
+		 }
+		 
+		 //Création tableau qui retransforme les valeurs de tab en String
+		 byte [] tabReturn = new byte [plainText.length];
+		 
+		 //Remplissage du Tableau
+		 for(int j=0;j<plainText.length;j++) {
+			 		 
+		 tabReturn [j] = Helper.bytesToString(tab[j]);
+		 }		 			 		 
+		
+		 		 	
+		 //Valeur attendue pour le cipherText: {-101, 32, -87, -109, -96, -90}
+			
+		 assert(plainText != null);
+			// TODO: COMPLETE THIS METHOD
+			
+			return tab; // TODO: to be modified
+		}
+		
 }
