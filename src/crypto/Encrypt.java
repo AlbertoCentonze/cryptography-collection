6c6074c9489937d6c22a92c972d8f326e0838668
Alberto <albicento.ac@gmail.com>
2020-10-24 07:57:09 +0200

    Small corrections and optimizations
    Now the caesar method return the exceptcted value. I've also replaced an if else statement with a switch one to improve performances. Give a look to my comment on your last commit (on Github website) for a more detailed explaination.

--- src/crypto/Encrypt.java
+++ src/crypto/Encrypt.java
@@ -48,38 +48,25 @@ public class Encrypt {
 		
 		
 		//Création tableau qui va changer les byte
-		 byte tab [] = new byte[plainText.length];
+		 byte tab[] = new byte[plainText.length];
 		 	
 		 //Remplissage du tableau 
-		 for(int i=0; i<plainText.length; i++) {
+		 for(int i = 0; i < plainText.length; i++) {
 			 	
-		//si l'on code l'espace "  "
-			 	if(spaceEncoding){
-				 
-				 tab[i]= (byte) (plainText[i] + (byte)key); 	
-				 
-			 	} else {
-		//si l'on ne code pas les epaces	 
-				 	if(plainText[i] != 32) {
-			
-				 		tab[i]= (byte) (plainText[i] + (byte)key);
-				 	} else { 
-				 		
-				 		tab[i]=32;
-				 	}
+		
+			 	if(spaceEncoding) //si l'on code l'espace "  "
+          tab[i]= (byte) (plainText[i] + (byte)key); 	
+        else { //si l'on ne code pas les epaces
+          switch(plainText[i]){
+            case (byte) 32:
+              tab[i] = 32;
+              break;
+            default:
+              tab[i] = (byte) (plainText[i] + (byte)key);
+          }
 			}
 		 }
 		 
-		 //Création tableau qui retransforme les valeurs de tab en String
-		 byte [] tabReturn = new byte [plainText.length];
-		 
-		 //Remplissage du Tableau
-		 for(int j=0;j<plainText.length;j++) {
-			 		 
-		 tabReturn [j] = Helper.bytesToString(tab[j]);
-		 }		 			 		 
-		
-		 		 	
 		 //Valeur attendue pour le cipherText: {-101, 32, -87, -109, -96, -90}
 			
 		 assert(plainText != null);
@@ -101,7 +88,7 @@ public class Encrypt {
 	 */
 	public static byte[] caesar(byte[] plainText, byte key) {
 		// TODO: COMPLETE THIS METHOD
-		return null; // TODO: to be modified
+		return caesar(plainText, key, true); // TODO: to be modified
 	}
 	
 	//-----------------------XOR-------------------------
