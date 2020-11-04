69a1edd15ef39d28e29a5dfe420833395d1f2731
JVRossier <73343229+JVRossier@users.noreply.github.com>
2020-11-04 20:50:49 +0100

    Updating the shell
    - Need to put the methods of Encrypt and Decrypt, I'll need your help for this part because I'm not sure of the way it can be done (litterally 2 minutes of work for this).
    -Lastly,  just need to test the shell to see if everything is OK (no case of coding or encoding forgotten  and see if there's problems with bugs).

--- src/crypto/Encrypt.java
+++ src/crypto/Encrypt.java
@@ -268,97 +268,205 @@ private static Scanner scanner = new Scanner(System.in);
     }
     return randomByteSequence;
   }
+  
+  
+  
+  
 public static void shell () {
 	
+	//Demande à l'utilisateur s'il veut coder ou décoder un message
+	String answer = asking();
 	
-	//demande code ou decode, recommence si reponse n'est pas correcte
-	String answer = ""; String s1 =  "code";  String s2 ="decode"; int i = 0;	
-	do {
-	System.out.println("Do you want to code a message or to decode a message ? (Write code or decode) : ");
-	 answer +=  scanner.nextLine();	
-	if(answer.equals(s1) || answer.equals(s2)){
-		++i;
-	}
-	}while(i==0);	
+	//Demande le message à l'utilisateur, demande tant qu'il n'est pas null.	
+	String message = message();
+		
+	String s1 = "code";  String s2 ="decode"; int i = 0;
+	
 	
 	//Si l'on code
 	if( answer.equals(s1) ) {
-				
-		//Demande le message à l'utilisateur, demande tant qu'il n'est pas null.
-		String message;
-		do {					
-		System.out.println("Write down the message you want to code : ");	     
-		message = scanner.nextLine();
-		}while(message!=null);
-		
-		
+											
 		//Savoir avec quelle methode, demande tant que la méthode est incorrecte
-		String methode;	String m1 = "CAESAR"; String m2 = "VIGENERE"; String m3 ="XOR"; String m4 ="ONETIME";  String m5 ="CBC";
-		do {					
-		System.out.println("Your message can be coded with Caesar, Vigenere, Xor, Onetime or CBC. Which one do you want ? (Write it in capital letters, ex : Xor => XOR) : ");
-		methode = scanner.nextLine();
-		if(answer.equals(m1) || answer.equals(m2)|| answer.equals(m3)|| answer.equals(m4)|| answer.equals(m5)){
-			++i;
-		}
-		} while(i==0);
+		String methode = methodeCode();
 		
-		//Avec CAESAR
-		if(methode.equals(m1)) {
+		String m1 = "CAESAR"; String m2 = "VIGENERE"; String m3 ="XOR"; String m4 ="ONETIME";  String m5 ="CBC";
+
+			//Avec CAESAR
+			if(methode.equals(m1)) {
 			
-			//Savoir la clé, tant qu'elle n'est pas nulle
-			System.out.println("Which key do you want to use (Write a number) ? : ");
-			if()
-		}
-		//Avec VIGENERE
-		else if (methode.equals(m2)){
+				int key = keylengthOf1 ();
+			}
+			//Avec VIGENERE
+			else if (methode.equals(m2)){
 			
-			//Savoir la clé, tant qu'elle n'est pas nulle		
-			System.out.println("Which key do you want to use (Write a number) ? : ");
-		}
-		//AvecXOR
-		else if (methode.equals(m3)){
+				
+				
+				
+				int keySize = keySize ();
+				
+				int tab[] = keyTab (keySize);
+			}
+			//AvecXOR
+			else if (methode.equals(m3)){
 			
-			//Savoir la clé, tant qu'elle n'est pas nulle
-			System.out.println("Which key do you want to use (Write a number) ? : ");
-		}
-		//Avec ONETIME
-		else if (methode.equals(m4)){
+				int key = keylengthOf1 ();
+			}
+			//Avec ONETIME
+			else if (methode.equals(m4)){
 			
-			//Savoir la clé, tant qu'elle n'est pas nulle
-			System.out.println("Which key do you want to use (Write a number) ? : ");
-		}
-		//Avec CBC
-		else if (methode.equals(m5)){
-			
-			//Savoir la clé, tant qu'elle n'est pas nulle
-		System.out.println("What's your key (Write a number):");
-		int key = 	
+				int tab[] =  keyOneTimePad(message);
+			}
+			//Avec CBC
+			else if (methode.equals(m5)){
 					
-		} 				
-	} 
+				
+				
+				int keySize = keySize ();
+				int tab[] = keyTab (keySize);
+			} 				
+		} 
+	
+	
+	
+	
 	//Si l'on decode
 	else if(answer.equals(s2)  ) {
-		String message;
+		
+		String methode = methodeDecode();
+
+		//Savoir avec quelle methode, demande tant que la méthode est incorrecte
+		String m1 = "caesarbruteforce"; String m2 = "caesarwithfrequencies"; String m3 ="xorbruteforce"; String m4 ="vigenerewithfrequencies";  String m5 ="decryptcbc";
+		
+			//Avec caesarbruteforce
+			if(methode.equals(m1)) {
+			
+						
+			} 			
+			//Avec caesarwithfrequencies
+			else if (methode.equals(m2)){
+			
+				
+			}
+			//Avec xorbruteforce
+			else if (methode.equals(m3)){
+			
+				
+			}
+			//Avec vigenerewithfrequencies
+			else if (methode.equals(m4)){
+			
+				
+			}
+			//Avec decryptcbc
+			else if (methode.equals(m5)){
+
+				
+				
+				int keySize = keySize ();
+				
+				int tab[] = keyTab (keySize);
+									
+			}
+			
+	}
+  }
+	public static String asking () {
+		
+		String answer = ""; String s1 =  "code";  String s2 ="decode"; int i = 0;		
+		do {
+			System.out.println("Do you want to code a message or to decode a message ? (Write code or decode) : ");
+			answer +=  scanner.nextLine();	
+				if(answer.equals(s1) || answer.equals(s2)){
+					++i;
+				}
+		}while(i==0);
+		return answer;
+	}
 	
-		//Demande le message à l'utilisateur, demande tant qu'il n'est pas null.
-		do {			
-			System.out.println("Write down the message you want to decode : ");
+	public static String message () {
+		
+		String message;
+		do {					
+			System.out.println("Write down the message you want to code : ");	     
 			message = scanner.nextLine();
 			}while(message!=null);
-		
+		return message;
+	}
+	
+	public static String  methodeCode () {
+	//Savoir avec quelle methode, demande tant que la méthode est incorrecte
+	
+		int i=0;		
+		String methode;	String m1 = "CAESAR"; String m2 = "VIGENERE"; String m3 ="XOR"; String m4 ="ONETIME";  String m5 ="CBC";
+			do {					
+				System.out.println("Your message can be coded with Caesar, Vigenere, Xor, Onetime or CBC. Which one do you want ? (Write it in capital letters, ex : Xor => XOR) : ");
+				methode = scanner.nextLine();
+					if(methode.equals(m1) || methode.equals(m2)|| methode.equals(m3)|| methode.equals(m4)|| methode.equals(m5)){
+						++i;
+					}
+			} while(i==0);
+			return methode;
+	}
+	
+	public static String methodeDecode () {
 		//Savoir avec quelle methode, demande tant que la méthode est incorrecte
-		String methode;	String m1 = "caesarbruteforce"; String m2 = "caesarwithfrequencies"; String m3 ="xorbruteforce"; String m4 ="vigenerewithfrequencies";  String m5 ="decryptcbc";
-		do {					
-		System.out.println("Your message can be decoded with CaesarBruteForce, CaesarWithFrequencies, XorBruteForce, VigenereWithFrequencies or DecryptCBC. Which one do you want ? (Write it in lowercase letter, ex : DecryptCBC => decryptcbc) : ");
-		methode = scanner.nextLine();
-		if(answer.equals(m1) || answer.equals(m2)|| answer.equals(m3)|| answer.equals(m4)|| answer.equals(m5)){
-			++i;
+		
+		int i=0;
+		String methode; String m1 = "caesarbruteforce"; String m2 = "caesarwithfrequencies"; String m3 ="xorbruteforce"; String m4 ="vigenerewithfrequencies";  String m5 ="decryptcbc";
+			do {					
+				System.out.println("Your message can be decoded with CaesarBruteForce, CaesarWithFrequencies, XorBruteForce, VigenereWithFrequencies or DecryptCBC. Which one do you want ? (Write it in lowercase letter, ex : DecryptCBC => decryptcbc) : ");
+				methode = scanner.nextLine();
+					if(methode.equals(m1) || methode.equals(m2)|| methode.equals(m3)|| methode.equals(m4)|| methode.equals(m5)){
+						++i;
+						}
+				} while(i==0);
+				return methode;
+	}
+	
+	public static int keylengthOf1 () {
+			
+		//Savoir la clé, tant qu'elle n'est pas nulle
+		System.out.println("Which key do you want to use (Write a number) ? : ");
+		int key= scanner.nextInt();
+		return key;
+	}
+	
+	public static int keySize () {
+		
+		//Savoir la clé, (tant qu'elle n'est pas nulle ??)
+		System.out.println("What is the size of your key ? : ");
+		int size= scanner.nextInt();
+		return size;
+	}
+	public static int [] keyTab (int keySize) {
+		
+		//Creation d'untableau qui condiendra les clés
+		System.out.println("The size of your message is : " +keySize);
+		System.out.println("You have to put one key number per letter of your message (write numbers) : ");
+		
+		int tabKey[] = new int [keySize];
+		for( int i = 0; i < keySize; ++i ) {
+			System.out.println();
+			tabKey[i]=scanner.nextInt();			
 		}
-		} while(i==0);
 	
+		return tabKey;
+	}
 	
+	public static int[] keyOneTimePad (String message) {
+		
+		//Une clé pour chque lettre (transformée en byte) du message
+		System.out.println("The size of your message is : " +message.length());
+		System.out.println("You have to put one key number per letter of your message (write numbers) : ");
+		
+		int tabKey[] = new int [message.length()];
+		for( int i = 0; i < message.length(); ++i ) {
+			System.out.println();
+			tabKey[i]=scanner.nextInt();			
+		}
+		
+		return tabKey;
+	}
 	
-  }
-}
-}
 
+}
